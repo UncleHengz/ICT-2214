60b9b167bc9d99e8ed3388667dffca1bbe3ac2f9
Yi Heng <yhchai2011@hotmail.com>
2024-02-21 17:12:15 +0800

    dont use the whois for now. trying dns lookup
    api quota hitting too fast. will find another one

--- domain_analysis/testing.py
+++ domain_analysis/testing.py
@@ -1,36 +1,39 @@
 import requests
+import dns.resolver
 
-def whois(domain):
-    url = "https://whois-lookup-service.p.rapidapi.com/v1/getwhois"
-    querystring = {"url": domain}
-    headers = {
-        "X-RapidAPI-Key": "e3da8e7390msha58253180e1c0d6p19545fjsn2f1f4b97e2cd",
-        "X-RapidAPI-Host": "whois-lookup-service.p.rapidapi.com"
-    }
+def dns_lookup(domain):
+    result = dns.resolver.resolve(domain, 'A')
+                
+# def whois(domain):
+#     url = "https://whois-lookup-service.p.rapidapi.com/v1/getwhois"
+#     querystring = {"url": domain}
+#     headers = {
+#         "X-RapidAPI-Key": "e3da8e7390msha58253180e1c0d6p19545fjsn2f1f4b97e2cd",
+#         "X-RapidAPI-Host": "whois-lookup-service.p.rapidapi.com"
+#     }
 
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()
+#     response = requests.get(url, headers=headers, params=querystring)
+#     data = response.json()
 
-    if data.get('error') is not None:
-        print(f"Error: {data['error']}")
-    else:
-        domain_data = data.get('data', {})
-        if domain_data:
-            for whois_server, details in domain_data.items():
-                print("Domain Information:")
-                for key, value in details.items():
-                    if isinstance(value, list):
-                        print(f"{key}:")
-                        for item in value:
-                            print(f"  - {item}")
-                    else:
-                        print(f"{key}: {value}")
-                print()
-        else:
-            print("No WHOIS information available for this domain.")
+#     if data.get('error') is not None:
+#         print(f"Error: {data['error']}")
+#     else:
+#         domain_data = data.get('data', {})
+#         if domain_data:
+#             for whois_server, details in domain_data.items():
+#                 print("Domain Information:")
+#                 for key, value in details.items():
+#                     if isinstance(value, list):
+#                         print(f"{key}:")
+#                         for item in value:
+#                             print(f"  - {item}")
+#                     else:
+#                         print(f"{key}: {value}")
+#                 print()
+#         else:
+#             print("No WHOIS information available for this domain.")
 
 # Example Usage
 domain = "singaporetech.edu.sg"
-whois(domain)
-
-
+# whois(domain)
+dns_lookup(domain)
