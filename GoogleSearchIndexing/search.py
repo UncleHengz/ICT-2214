4da242d5831898487bc44c451284d894a7655937
nlct2k <59596071+nlct2k@users.noreply.github.com>
2024-03-05 10:29:24 +0800

    attempt to get data from similarweb  , getting back my spoilt laptop so hopefully can do more

--- GoogleSearchIndexing/search.py
+++ GoogleSearchIndexing/search.py
@@ -1,9 +1,15 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# similiarweb api key:0ec16759045a4290800ff27abebc07b6
 
 import requests
+from urllib.parse import urlparse
+def extract_domain(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    if domain.startswith('www.'):
+        domain = domain[4:]
+    return domain
+
 
 def check_site_google(url):
     api_key = "AIzaSyDJmKncAKqwTofjx3JhdhhVGcQK0eZ3yrU" # google custom search json api key
@@ -11,7 +17,12 @@ def check_site_google(url):
     query = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={cse_id}&q={url}"
     response = requests.get(query)
     results = response.json()
-    # print(results)
+    print(results["items"])
+    for i in results["items"]:
+        print("_________")
+        for x in i:
+
+            print(x,":",i[x])
 
     # Check if there are any search results
     if results.get("searchInformation") and int(results.get("searchInformation").get("totalResults", 0)) > 0:
@@ -19,9 +30,18 @@ def check_site_google(url):
     else:
         return False, "The site is not indexed by Google."
 
+def print_dict(d, indent):
+    for key, value in d.items():
+        print(' ' * indent + str(key) + ':', end=' ')
+        if isinstance(value, dict):
+            print()  # Print a newline if the value is a dictionary
+            print_dict(value, indent + 4)  # Recursively print the nested dictionary with increased indentation
+        else:
+            print(value)  # Print the value
 
+ # todo: main thing is to find ranking of site
 if __name__ == '__main__':
-    is_indexed, message = check_site_google("http://www.cloud4sal3xoxoMeow.com/")
+    is_indexed, message = check_site_google("https://amaonz.xjijin.com.cn")
     #can try with other sites
     #isIndexed is a boolean
     #message is whether indexed
