402a533a95b96db7d8bdc64a45180d9a8c6b60b9
nlct2k <59596071+nlct2k@users.noreply.github.com>
2024-03-05 18:02:06 +0800

    not good

--- GoogleSearchIndexing/search.py
+++ GoogleSearchIndexing/search.py
@@ -10,8 +10,17 @@ def extract_domain(url):
         domain = domain[4:]
     return domain
 
+siteinfo = {
+    "url":"",
+    "information":
+    {
+        "indexd":"no",
+        "ranking": 0
+    }
+}
 
 def check_site_google(url):
+
     api_key = "AIzaSyDJmKncAKqwTofjx3JhdhhVGcQK0eZ3yrU" # google custom search json api key
     cse_id = "a6d5af5008e9e4d57" #custom search engine id
     query = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={cse_id}&q={url}"
@@ -41,9 +50,38 @@ def print_dict(d, indent):
 
  # todo: main thing is to find ranking of site
 if __name__ == '__main__':
-    is_indexed, message = check_site_google("https://amaonz.xjijin.com.cn")
+    # is_indexed, message = check_site_google("https://amaonz.xjijin.com.cn")
+    test=extract_domain('www.mofunzone.com')
+    print(test)
     #can try with other sites
     #isIndexed is a boolean
     #message is whether indexed
 
-    print(message)
+
+
+# -----------
+# import requests
+
+#wanted to try similarweb api but expired api (7 days)
+# url = "https://api.similarweb.com/v3/batch/traffic_and_engagement/request-report"
+
+# payload = {
+#     "metrics": ["all_traffic_visits", "global_rank", "desktop_new_visitors", "mobile_average_visit_duration"],
+#     "filters": {
+#     },
+#     "granularity": "monthly",
+#     "response_format": "csv",
+#     "delivery_method": "download_link"
+# }
+# headers = {
+#     "accept": "application/json",
+#     "content-type": "application/json",
+#     "api-key": "a6c8066af27840b2a37cf72fbe6ec2b9"
+# }
+
+# response = requests.post(url, json=payload, headers=headers)
+
+# print(response.text)
+    
+
+    #googlesearchconsole
