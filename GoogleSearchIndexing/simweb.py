fd5c7f4614e9f36e8cb934a5972ab004a20ef306
nlct2k <59596071+nlct2k@users.noreply.github.com>
2024-03-06 13:49:50 +0800

    Added neccessary fields for data

--- GoogleSearchIndexing/simweb.py
+++ GoogleSearchIndexing/simweb.py
@@ -1,8 +1,7 @@
 try:
     from requests import get
     from urllib.parse import urlparse
-    import random
-    
+    import random   
 
 except ImportError as err:
     print(f"Failed to import required modules {err}")
@@ -12,6 +11,18 @@ user_agents_list = [ # allow us to bypass 403 error
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
 ]
+
+   
+def pretty_print_dict(d, indent=0):
+    res = ""
+    for k, v in d.items():
+        res += "\t"*indent + str(k) + "\n"
+        if isinstance(v, dict):
+            res += pretty_print_dict(v, indent+1)
+        else:
+            res += "\t"*(indent+1) + str(v) + "\n"
+    return res
+
 
 def similarGet(website):
     domain = '{uri.netloc}'.format(uri=urlparse(website))
@@ -25,20 +36,48 @@ def similarGet(website):
         resp.raise_for_status()
         return False
     
-result = similarGet("http://www.facebook.com")
+result = similarGet("https://www.facebook.com")
 filtDict ={
     "SiteName":"",
     "TopCountryShares":[], # can find out top 5 country affected , only need country code
-    "Engagments":"",
-    "AvgMonthlyVisits":0,
+    "TotalVists":0,
+    "Category":"",
     "GlobalRank":0,
+    "HQCountry":"",
     "CountryRank":0,
     "TrafficSources":{},
     "CountryCount":0, # From Countries key
-    "SearchTerms":[]
+    "SearchTerms":[] # From TopKeywords
 }
-for i in result:
-    print(i) #check key names
-print()
-print(result["EstimatedMonthlyVisits"]) # Check data
+
+print(result["CountryRank"]) # Check data
+
 filtDict["SiteName"]= result["SiteName"]
+
+for i in result["TopCountryShares"]:
+    filtDict["TopCountryShares"].append(i["CountryCode"])
+
+filtDict["TotalVists"] = result["Engagments"]["Visits"]
+
+filtDict["Category"] = result["Category"]
+
+filtDict["GlobalRank"] = result["GlobalRank"]
+
+filtDict["HQCountry"] = result["CountryRank"]["CountryCode"]
+
+filtDict["CountryRank"] = result["CountryRank"]["Rank"]
+
+filtDict["TrafficSources"] = result["TrafficSources"]
+
+countryCount = 0
+
+for i in result["Countries"]:
+    countryCount += 1
+
+filtDict["CountryCount"] = countryCount
+
+# filtDict["SearchTerms"]
+for i in result["TopKeywords"]:
+    filtDict["SearchTerms"].append(i["Name"])
+
+# print(pretty_print_dict(filtDict))
