f2b277b0eecc0311c5e1a1b3ba618c8e759a792e
Edkaa <e-svh@hotmail.com>
2024-03-31 17:25:32 +0800

    folder restructure

--- server/copy_updated_db.py
+++ server/copy_updated_db.py
@@ -4,7 +4,7 @@ import os
 import tarfile
 
 def copy_file():
-    source_directory = "/home/suibiandeming/ICT-2214/external_database_update/Links"  # Replace this with the actual source directory
+    source_directory = "external_database_update/Links"  # Replace this with the actual source directory
     filename_to_check = "ALL-phishing-links.txt"  # Replace this with the actual filename
 
     source_path = os.path.join(source_directory, filename_to_check)
